9323a99f943f7b58e990afa22a664a7891fb4d7d
eLoopWoo <eyzenberg.tomer@gmail.com>
2021-06-20 18:26:33 +0300

    wip(connection.py): more connection logic

--- src/connection.py
+++ src/connection.py
@@ -1,3 +1,22 @@
+import socket
+
+from const import Consts
+
+
 class Connection(object):
     def __init__(self, sock):
         self.sock = sock
+
+    def is_closed(self):
+        data = self.sock.recv(16, socket.MSG_DONTWAIT | socket.MSG_PEEK)
+        if len(data) == 0:
+            self.sock.close()
+
+    def send(self, msg):
+        self.sock.send(msg)
+
+    def recv(self, size=Consts.MAX_MSG_LENGTH):
+        self.sock.recv(size)
+
+    def close(self):
+        self.sock.close()
--- src/server.py
+++ src/server.py
@@ -3,6 +3,7 @@ import socket
 from threading import Thread
 
 from connection import Connection
+from connections import Connections
 from const import Consts
 
 
@@ -13,14 +14,18 @@ class Server(object):
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-        self.connections = {}
+        self.server.bind((self.ip, self.port))
+        self.server.listen(Consts.MAX_SERVER_LISTEN)
+
+        self.thread_accept = None
+        self.thread_data = None
+        self.thread_manage = None
+
+        self.connections = Connections()
         # [1,2,3,4,5,6,7,8,9,10] (O(n))
         # {'1': 1, ... , '9': 9 } (O(1))
 
     def start(self):
-        self.server.bind((self.ip, self.port))
-        self.server.listen(Consts.MAX_SERVER_LISTEN)
-
         thread_accept = Thread(target=self._handle_accept)
         thread_accept.start()
 
@@ -36,35 +41,39 @@ class Server(object):
         exit(1)
 
     def _broadcast(self, msg, origin):
-        socks = list(self.connections.keys())
-        for sock in socks:
-            if sock == origin:
+        connections = list(self.connections.get_connections())
+        for connection in connections:
+            if connection == origin:
                 continue
-            sock.send(msg)
+            # TODO: Might throw exception if the socket was closed already *race*
+            connection.send(msg)
 
     def _handle_accept(self):
         while True:
             sock, (ip, port) = self.server.accept()
-            con = Connection(sock)
-            self.connections[sock] = con
+            connection = Connection(sock)
+            self.connections.add(connection)
             print('New client: from {}:{}'.format(ip, port))
 
     def _handle_data(self):
         while True:
             # TODO: Might race if not (list)
-            socks = list(self.connections.keys())
+            socks = list(self.connections.get_socks())
             ready_to_read, _, _ = select.select(socks, [], [], Consts.TIMEOUT_SELECT_DATA)
 
             for sock in ready_to_read:
-                data = sock.recv(16, socket.MSG_DONTWAIT | socket.MSG_PEEK)
-                if len(data) == 0:
-                    sock.close()
-                    self.connections.pop(sock)
-                data = sock.recv(Consts.MAX_MSG_LENGTH)
-                self._broadcast(msg=data, origin=sock)
+                connection = self.connections.get(sock)
+                self._handle_connection(connection)
 
     def _handle_manage(self):
         while True:
             choice = input('enter "exit" to terminate')
             if choice == 'exit':
                 return True
+
+    def _handle_connection(self, connection):
+        if connection.is_closed():
+            connection.close()
+            self.connections.remove(connection)
+        data = connection.recv(Consts.MAX_MSG_LENGTH)
+        self._broadcast(msg=data, origin=connection)
